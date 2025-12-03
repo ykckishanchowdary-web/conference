b935b7184aa7aa1a1d582301be34ca0aacca21da
beastthor <beastthor.ykc@gmail.com>
2025-12-03 14:24:31 +0530

    implement WebMvcConfigurer and ovveride the method in it

--- src/main/java/com/learning/conference/ConferenceConfig.java
+++ src/main/java/com/learning/conference/ConferenceConfig.java
@@ -4,10 +4,21 @@ package com.learning.conference;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class ConferenceConfig {
+public class ConferenceConfig implements WebMvcConfigurer {
+
+
+    //If wanted the visit the file of your choice use WebMvcConfigurer and override addResourceHandlers method
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf");
+
+    }
 
 
     @Bean
